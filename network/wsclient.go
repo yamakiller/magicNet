82738edd43980520fa5be52a09c9b131f0c5ca7f
yamakiller <296270991@qq.com>
2019-07-16 19:25:09 +0800

    websocket 客户端

--- network/wsclient.go
+++ network/wsclient.go
@@ -2,7 +2,9 @@ package network
 
 import (
 	"magicNet/engine/actor"
+	"magicNet/timer"
 	"sync"
+	"sync/atomic"
 
 	"github.com/gorilla/websocket"
 )
@@ -30,16 +32,89 @@ func (wsc *wsclient) connect(operator *actor.PID, addr string) error {
 	}
 
 	wsc.c = c
-	//启动读数据协程
-	//启动写数据协程
-
 	return nil
 }
 
-func (wsc *wsclient) read() {
+func (wsc *wsclient) read(so *slot) {
+	for {
+		if wsc.stat != Connecting && wsc.stat != Connected {
+			goto read_end
+		}
 
+		msgType, data, err := wsc.c.ReadMessage()
+		if err != nil {
+			//记录错误日志
+			goto read_error
+		}
+
+		// 不接收非二进制编码数据
+		if msgType != websocket.BinaryMessage {
+			goto read_error
+		}
+
+		// 丢弃数据包
+		if wsc.stat != Connected {
+			continue
+		}
+
+		wsc.i.ReadBytes += uint64(len(data))
+		wsc.i.ReadLastTime = timer.Now()
+		//数据包丢给 Actor
+		actor.DefaultSchedulerContext.Send(wsc.o, &NetChunk{Data: data})
+	}
+
+read_error:
+	wsc.stat = Closing
+	wsc.c.Close()
+read_end:
+	var (
+		closeHandle   int32
+		closeOperator *actor.PID
+	)
+
+	so.l.Lock()
+	closeHandle = wsc.h
+	closeOperator = wsc.o
+	close(wsc.out)
+	//-----等待写协程结束------
+	for {
+		if atomic.CompareAndSwapInt32(&wsc.outStat, 1, 1) {
+			break
+		}
+	}
+	so.s = nil
+	so.b = resIdle
+	so.l.Unlock()
+
+	wsc.w.Done()
+
+	actor.DefaultSchedulerContext.Send(closeOperator, &NetClose{handle: closeHandle})
 }
 
-func (wsc *wsclient) write() {
+func (wsc *wsclient) write(so *slot) {
+	for {
+		if wsc.stat != Connecting && wsc.stat != Connected {
+			goto write_end
+		}
 
+		select {
+		case msg := <-wsc.out:
+			if wsc.stat != Connecting && wsc.stat != Connected {
+				goto write_end
+			}
+
+			if err := wsc.c.WriteMessage(websocket.BinaryMessage, msg.Data); err != nil {
+				goto write_error
+			}
+
+			wsc.i.WriteBytes += uint64(len(msg.Data))
+			wsc.i.WriteLastTime = timer.Now()
+		}
+	}
+
+write_error:
+	wsc.stat = Closing
+write_end:
+	wsc.w.Done()
+	wsc.outStat = 1
 }
