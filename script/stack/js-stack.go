9c6863f47f6e25874535c9a6fb34908f69f16942
yamakiller <296270991@qq.com>
2019-11-13 22:02:07 +0800

    fixed Comment

--- script/stack/js-stack.go
+++ script/stack/js-stack.go
@@ -15,47 +15,71 @@ var (
 	ErrJSNotFileData = errors.New("did not get file data")
 )
 
-// JSStack : javascirpt 虚拟器
+//JSStack desc
+//@struct JSStack desc: javascirpt virtual machine
 type JSStack struct {
 	state *otto.Otto
 }
 
-// MakeJSStack : 制作 JS 虚拟机
+//MakeJSStack desc
+//@method MakeJSStack desc: create javascript virtual machine
 func MakeJSStack() *JSStack {
 	return &JSStack{otto.New()}
 }
 
-// SetInt : 给JS脚本设置Int变量
+//SetInt desc
+//@method SetInt desc: Set the Int variable to the JS script
+//@param (string) name
+//@param (int)    value
 func (slf *JSStack) SetInt(name string, val int) {
 	slf.state.Set(name, val)
 }
 
-// SetFloat : 给JS脚本设置Float 32 变量
+//SetFloat desc
+//@method SetFloat desc: Set the Float 32 variable to the JS script
+//@param (string)     name
+//@param (float32)    value
 func (slf *JSStack) SetFloat(name string, val float32) {
 	slf.state.Set(name, val)
 }
 
-// SetDouble :  给JS脚本设置Float 64 变量
+//SetDouble desc
+//@method SetDouble desc: Set the Float 64 variable to the JS script
+//@param (string)     name
+//@param (float64)    value
 func (slf *JSStack) SetDouble(name string, val float64) {
 	slf.state.Set(name, val)
 }
 
-// SetBoolean : 给JS脚本设置Bool 变量
+//SetBoolean desc
+//@method SetBoolean desc: Set Bool variables for JS scripts
+//@param (string)     name
+//@param (bool)       value
 func (slf *JSStack) SetBoolean(name string, val bool) {
 	slf.state.Set(name, val)
 }
 
-// SetString : 给JS脚本设置String变量
+//SetString desc
+//@method SetString desc: Set String variables for JS scripts
+//@param (string)     name
+//@param (string)       value
 func (slf *JSStack) SetString(name string, val string) {
 	slf.state.Set(name, val)
 }
 
-// SetFunc : 设置js脚本调用Go的函数
+//SetFunc desc
+//@method SetFunc desc: Set the js script to call Go's function
+//@param (string)       name
+//@param (interface{})  value
 func (slf *JSStack) SetFunc(name string, fun interface{}) {
 	slf.state.Set(name, fun)
 }
 
-// ExecuteScriptFile : 执行脚本文件
+//ExecuteScriptFile desc
+//@method ExecuteScriptFile desc: Execution script file
+//@param   (string) scirpt file path
+//@return  (otto.Value) javascript result
+//@return  (error) javascript execution error result
 func (slf *JSStack) ExecuteScriptFile(filename string) (otto.Value, error) {
 	tmpFileName := files.GetFullPathForFilename(filename)
 	if !files.IsFileExist(tmpFileName) {
