fc5a323b8a4687c756003de9836f795464a2bb61
unknown <296270991@qq.com>
2019-11-13 16:55:25 +0800

    fixed service instance/base class

--- service/service.go
+++ service/service.go
@@ -11,10 +11,31 @@ import (
 	"github.com/yamakiller/magicNet/util"
 )
 
-// MethodFunc : Service method function
+//MethodFunc desc
+//@type MethodFunc desc: Service method function
+//@param (actor.Context) a actor context
+//@param (actor.PID) sender actor ID
+//@param (interface{}) a message
 type MethodFunc func(self actor.Context, sender *actor.PID, message interface{})
 
-// IService  Service base class interface
+//IService desc
+//@interface IService desc: Service base class interface
+//@inherit (actor.Actor)
+//@method (GetPID() *actor.PID ) return this id
+//@method (Name() string) return this name
+//@method (Key() string) return this pid is string
+//@method (ID() uint32) return this pid=>id
+//@method (Init()) initialization this
+//@method (Shutdown) shutdown this service
+//@method (RegisterMethod) register event call method
+//@method (withPID(context actor.Context)) assignment this pid
+//@method (withName)
+//@method (widthWait)
+//@method (LogInfo)
+//@method (LogError)
+//@method (LogDebug)
+//@method (LogTrace)
+//@method (LogWarning)
 type IService interface {
 	actor.Actor
 
@@ -24,13 +45,11 @@ type IService interface {
 	ID() uint32
 
 	Init()
-	Assignment(context actor.Context)
 	Shutdown()
-
 	RegisterMethod(key interface{}, method MethodFunc)
 
+	withPID(context actor.Context)
 	withName(n string)
-
 	withWait(wait *sync.WaitGroup)
 
 	LogInfo(frmt string, args ...interface{})
@@ -40,7 +59,12 @@ type IService interface {
 	LogWarning(frmt string, args ...interface{})
 }
 
-// Service server base class
+//Service desc
+//@struct Service desc: server base class
+//@member (*actor.PID) this id
+//@member (string) this server name
+//@member (*sync.WaitGroup)
+//@member (map[interface{}]MethodFunc) event method map
 type Service struct {
 	pid    *actor.PID
 	name   string
@@ -48,7 +72,8 @@ type Service struct {
 	method map[interface{}]MethodFunc
 }
 
-// Init : Initialization service
+//Init desc
+//@method Init desc: Initialization service
 func (slf *Service) Init() {
 	slf.method = make(map[interface{}]MethodFunc, 16)
 	slf.RegisterMethod(&actor.Started{}, slf.Started)
@@ -57,7 +82,9 @@ func (slf *Service) Init() {
 	slf.RegisterMethod(&actor.Terminated{}, slf.Terminated)
 }
 
-// Receive : Receive message
+//Receive desc
+//@method Receive desc: Receive message and Scheduling
+//@param (actor.Context) source actor context
 func (slf *Service) Receive(context actor.Context) {
 	switch msg := context.Message().(type) {
 	case *actor.MessagePack:
@@ -76,30 +103,44 @@ func (slf *Service) Receive(context actor.Context) {
 	}
 }
 
-//Assignment Service initial value association
-func (slf *Service) Assignment(context actor.Context) {
+//withPID desc
+//@method withPID desc: Service initial value association
+//@param (actor.Context) this service context
+func (slf *Service) withPID(context actor.Context) {
 	slf.pid = context.Self()
 	slf.name = slf.name + "$" + strconv.Itoa(int(slf.pid.ID))
 }
 
-// Started : Service start function
+//Started desc
+//@method Started desc: Started Event Call Function
+//@param (actor.Context) source actor context
+//@param (*actor.PID) sender actor ID
+//@param (interface{}) a message
 func (slf *Service) Started(context actor.Context,
 	sender *actor.PID,
 	message interface{}) {
 
 	if slf.pid == nil {
-		slf.Assignment(context)
+		slf.withPID(context)
 	}
 	if slf.wait != nil {
 		slf.wait.Done()
 	}
 }
 
-//Stopping : Service stopped
+//Stopping desc
+//@method Stopping desc: Stopping Event Call Function
+//@param (actor.Context) source actor context
+//@param (*actor.PID) sender actor ID
+//@param (interface{}) a message
 func (slf *Service) Stopping(context actor.Context, sender *actor.PID, message interface{}) {
 }
 
-// Stoped : Service stop closing handler
+//Stoped desc
+//@method Stopping desc: Stopped Event Call Function
+//@param (actor.Context) source actor context
+//@param (*actor.PID) sender actor ID
+//@param (interface{}) a message
 func (slf *Service) Stoped(context actor.Context, sender *actor.PID, message interface{}) {
 	for k := range slf.method {
 		delete(slf.method, k)
@@ -110,12 +151,17 @@ func (slf *Service) Stoped(context actor.Context, sender *actor.PID, message int
 	}
 }
 
-// Terminated : The service is terminated and can be destroyed
+//Terminated desc
+//@method Terminated desc: Terminated Event Call Function
+//@param (actor.Context) source actor context
+//@param (*actor.PID) sender actor ID
+//@param (interface{}) a message
 func (slf *Service) Terminated(context actor.Context, sender *actor.PID, message interface{}) {
-	slf.Shutdown()
+	//slf.Shutdown()
 }
 
-// Shutdown : Proactively shut down the service
+//Shutdown desc
+//@method Shutdown desc: Shutdown service
 func (slf *Service) Shutdown() {
 	if slf.pid == nil {
 		return
@@ -124,12 +170,16 @@ func (slf *Service) Shutdown() {
 	slf.wait.Wait()
 }
 
-// Name : Get the name of the service
+//Name desc
+//@method Name desc: Return the name of the service
+//@return (string) name
 func (slf *Service) Name() string {
 	return slf.name
 }
 
-// Key : Returns the Key name of the service
+//Key desc
+//@method Key desc: Returns the Key name of the service
+//@return (string) pid=>key
 func (slf *Service) Key() string {
 	ix := strings.IndexByte(slf.name, '$')
 	if ix <= 0 {
@@ -139,17 +189,24 @@ func (slf *Service) Key() string {
 	return util.SubStr2(slf.name, 0, ix)
 }
 
-//GetPID Return the pid object
+//GetPID desc
+//@method GetPID desc: Return the pid object
+//@return (*actor.PID) actor ID
 func (slf *Service) GetPID() *actor.PID {
 	return slf.pid
 }
 
-// ID Returns the unique number of the service
+//ID desc
+//@method ID desc: Returns this service pid=>id
+//@return (uint32) ID
 func (slf *Service) ID() uint32 {
 	return slf.pid.ID
 }
 
-// RegisterMethod : Registration (convention/agreement) method
+//RegisterMethod desc
+//@method RegisterMethod desc: Registration (convention/agreement) method
+//@param (interface{}) event map key
+//@param (MethodFunc) Function object
 func (slf *Service) RegisterMethod(key interface{}, method MethodFunc) {
 	slf.method[reflect.TypeOf(key)] = method
 }
@@ -162,32 +219,51 @@ func (slf *Service) withWait(wait *sync.WaitGroup) {
 	slf.wait = wait
 }
 
-//LogInfo Log information
+//LogInfo desc
+//@method LogInfo desc: Log information
+//@param  (string) format string
+//@param  (...interface{}) format args
 func (slf *Service) LogInfo(frmt string, args ...interface{}) {
 	logger.Info(slf.ID(), frmt, args...)
 }
 
-//LogError Record error log information
+//LogError desc
+//@method LogError desc: Record error log information
+//@param  (string) format string
+//@param  (...interface{}) format args
 func (slf *Service) LogError(frmt string, args ...interface{}) {
 	logger.Error(slf.ID(), frmt, args...)
 }
 
-//LogDebug Record debug log information
+//LogDebug desc
+//@method LogDebug desc: Record debug log information
+//@param  (string) format string
+//@param  (...interface{}) format args
 func (slf *Service) LogDebug(frmt string, args ...interface{}) {
 	logger.Debug(slf.ID(), frmt, args...)
 }
 
-//LogTrace Record trace log information
+//LogTrace desc
+//@method LogTrace desc: Record trace log information
+//@param  (string) format string
+//@param  (...interface{}) format args
 func (slf *Service) LogTrace(frmt string, args ...interface{}) {
 	logger.Trace(slf.ID(), frmt, args...)
 }
 
-//LogWarning Record warning log information
+//LogWarning desc
+//@method LogWarning desc: Record warning log information
+//@param  (string) format string
+//@param  (...interface{}) format args
 func (slf *Service) LogWarning(frmt string, args ...interface{}) {
 	logger.Warning(slf.ID(), frmt, args...)
 }
 
-// Make : Service creator
+//Make desc
+//@method Make desc: Service creator function
+//@param (string) service name
+//@param (func() IService) service maker(function)
+//@param (IService) service
 func Make(name string, f func() IService) IService {
 	wgn := &sync.WaitGroup{}
 	srv := f()
